0ec09e49d5a13085f62ff3a4ca056251e2351d50
uday surya <udaysurya1597@gmail.com>
2025-03-18 10:28:31 +0530

    learning route handling

--- src/app.js
+++ src/app.js
@@ -2,11 +2,49 @@ const express = require('express');
 
 const app = express();
 
+const r1 = (req, res, next) => {
+    console.log('getroute 1');
+    next();
+    // res.send('enroute to swizz')
+}
+
+const r2 = (req, res, next) => {
+    console.log('getroute 2');
+    next();
+    // res.send('we are here');
+}
+
+const r3 = (req, res, next) => {
+    console.log('getroute 3');
+    next();
+    // res.send('lalalal land');
+}
+
 
 //request handler
-app.use((req, res) => {
-    res.send('Hello World, yes');
-})
+app.use("/users",(req,res, next) => {
+    console.log('route' );
+    // res.send('route handler one');
+    next();
+    
+},(req, res,next) => {
+    console.log('route 2');
+    // res.send('route handler two');
+    next();
+    res.send('there you are');
+}),(req, res,next) => {
+    console.log('route 3');
+    res.send('route handler three');
+    next();
+},(req, res,next) => {
+    console.log('route 4');
+    res.send('route handler four');
+    next();
+}
+
+app.use('/getter', r1, r2, r3, (req, res) => {
+    res.send('route handler five');
+});
 
 app.listen(8010, () => {
     console.log('Server is running on port 8010');
