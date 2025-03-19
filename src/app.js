167b78191525028f3502b0bd45dfb8768421e8a7
uday surya <udaysurya1597@gmail.com>
2025-03-19 13:06:28 +0530

    learning mongoose and a new model

--- src/app.js
+++ src/app.js
@@ -1,8 +1,27 @@
 const express = require('express');
 const connectDB = require('./config/database');
+const User = require('./models/user');
 require('./config/database');
 
 const app = express();
+
+app.post('/signup',(req, res) => {
+    const user = new User({
+        firstName : 'Krishna',
+        lastName: "bujji",
+        emailId: 'krishna33@gmail.com',
+        password: 'krishna333',
+        age: 3,
+        gender: 'male'
+    });
+    user.save().then(() => {
+        res.send(user);
+    }).catch((err) => {
+        res.send(err);
+    })
+})
+
+
 
 connectDB().then(() => {
     console.log('Database connected.....');
--- src/config/database.js
+++ src/config/database.js
@@ -1,8 +1,14 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://aparnavenugopal1996:Targetcat430@cluster0.zbjjj.mongodb.net/?authSource=admin')
-}
+  try {
+    await mongoose.connect(
+      'mongodb+srv://aparnavenugopal1996:Targetcat430@cluster0.zbjjj.mongodb.net/Krishinder?retryWrites=true&w=majority'
+    );
+    console.log('Database connection established...');
+  } catch (err) {
+    console.error('MongoDB connection error:', err.message);
+  }
+};
 
 module.exports = connectDB;
-
