925708ce78e583fe0aafbe670418a41f27c9d362
uday surya <udaysurya1597@gmail.com>
2025-03-22 16:50:26 +0530

    checks for models 1

--- src/models/user.js
+++ src/models/user.js
@@ -2,23 +2,69 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
     firstName:{
-        type: String
+        type: String,
+        required : true,
+        minlength: 3,
+        maxlength: 50,
+        trim: true
     },
     lastName:{
-        type: String
+        type: String,
+        trim: true
     },
     emailId: {
-        type: String
+        type: String,
+        unique: true,
+        required : true,
+        lowercase: true,
+        trim: true,
+        validate : {
+            validator : function(value){
+                return /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(value);
+            },
+            message : 'Invalid email format!!!'
+        }     
     },
     password: {
-        type: String
+        type: String,
+        required: true,
+        trim: true,
+        minlength : 3,
+        validate : {
+            validator : function(value){
+                return /[0-9]/.test(value); 
+            },
+            message : 'password should contain atleast one number'
+        }
     },
     age:{
         type:   Number
     },
     gender:{
-        type: String
+        type: String,
+        required: true ,
+        set: v => v.trim().toLowerCase(),
+        validate : {
+            validator : function(value){
+                return ['male', 'female','others'].includes(value);
+            },
+            message: 'gender must be male, female or others'
+        }
+
     },
+    phoneNumber : {
+        type : Number
+    },
+    about : {
+        type : String ,
+        default : "Hey there! I am  a fellow developer , lets connect",
+    },
+    skills : {
+        type : [String],
+
+    }
+},{
+    timestamps : true
 });
 
 const User = mongoose.model('User', userSchema);
