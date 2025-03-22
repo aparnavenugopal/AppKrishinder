c747c3bb81fb585e9416c7c818b3c7cde07fddc4
uday surya <udaysurya1597@gmail.com>
2025-03-22 16:50:14 +0530

    checks for models

--- src/app.js
+++ src/app.js
@@ -13,10 +13,57 @@ app.post('/signup',async (req, res) => {
       await user.save();
       res.status(201).send('user successfully added');
    }catch(e){
-        res.status(400).send('error while adding user',e);
+        res.status(400).send({ error: 'Error while adding user', details: e.message });
    }
 })
 
+app.patch('/update', async (req, res) => {
+    try{
+        const user = await User.findByIdAndUpdate(req.body._id,req.body);
+        if(!user){
+            res.status(404).send('user cant be updatd as no record found with this id');
+        }
+        res.status(201).send('user successfully updated');
+    }catch(e){
+        res.status(400).send({ error: 'Error while updating user', details: e.message });
+    }
+})
+
+app.get('/user',async (req, res) => {
+    try{
+        const firstName =  await User.findOne({firstName : req.body.firstName});
+        if(!firstName){
+            res.status(404).send('user not found');
+        }
+        res.status(200).send(firstName);
+    }catch(e){
+        res.status(400).send({ error: 'Error while getting user', details: e.message });
+    }
+})
+
+app.get('/feed',async (req, res) => {
+   try{
+        const user = await User.find();
+        res.status(200).send(user);
+        if(!user){
+            res.status(404).send('user not found');
+        }
+   }catch(e){
+    res.status(400).send({ error: 'Error while getting user', details: e.message });
+   }
+})
+
+app.delete('/delete', async(req, res) => {
+    try{
+        const user = await User.findByIdAndDelete(req.body._id);
+        if(!user){
+            res.status(404).send('user not found');
+        }
+        res.status(200).send('user successfully deleted!');
+    }catch(e){
+        res.status(400).send({ error: 'Error while deleting the user', details: e.message });
+    }
+})
 
 
 connectDB().then(() => {
